c72e4a6a4b8062626017151955e674a9e4abe928
Allen, Timothy <tallen@wharton.upenn.edu>
2017-10-21 21:55:50 +0000

    Declare migration bankruptcy, add header image and excerpt.

--- peregrine/models.py
+++ peregrine/models.py
@@ -1,19 +1,44 @@
 from django.conf import settings
 from django.db import models
+from django.utils.timezone import now
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtailcontentstream.models import ContentStreamPage
 
 
 class Page(ContentStreamPage):
-    pass
-
-
-class Post(ContentStreamPage):
-    date = models.DateField("Post Date")
-    authors = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    header_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    excerpt = RichTextField(
+        features=['bold', 'italic', 'link', 'ol', 'ul'],
+        blank=True,
+        null=True,
+    )
 
     content_panels = [
-        FieldPanel('authors'),
-        FieldPanel('date'),
+        ImageChooserPanel('header_image'),
+        FieldPanel('excerpt'),
     ] + ContentStreamPage.content_panels
+
+
+class Post(Page):
+    post_date = models.DateTimeField(
+        default=now,
+    )
+    authors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+    )
+
+    content_panels = [
+        FieldPanel('post_date'),
+        FieldPanel('authors'),
+    ] + Page.content_panels
