58f0dea9dbf81e76b81be1800b0f83dcdc076362
Allen, Timothy <tallen@wharton.upenn.edu>
2017-10-21 22:19:19 +0000

    Add help text. Default pages to show in site menus. Add verbose names.

--- peregrine/models.py
+++ peregrine/models.py
@@ -8,37 +8,53 @@ from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtailcontentstream.models import ContentStreamPage
 
 
-class Page(ContentStreamPage):
+class SitePage(ContentStreamPage):
     header_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+',
+        help_text='A featured image that will appear in the site theme header.',
     )
     excerpt = RichTextField(
         features=['bold', 'italic', 'link', 'ol', 'ul'],
         blank=True,
         null=True,
+        help_text='An short excerpt or abstract about the content.'
     )
+    show_in_menus_default = True
 
     content_panels = [
+        FieldPanel('title'),
         ImageChooserPanel('header_image'),
         FieldPanel('excerpt'),
-    ] + ContentStreamPage.content_panels
+        FieldPanel('body'),
+    ]
+
+    class Meta:
+        verbose_name = 'Page'
 
 
-class Post(Page):
+class SitePost(SitePage):
     post_date = models.DateTimeField(
         default=now,
+        help_text='The date and time of the post.',
     )
     authors = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         blank=True,
-        null=True,
+        help_text='The authors of the post.',
     )
 
     content_panels = [
+        FieldPanel('title'),
         FieldPanel('post_date'),
         FieldPanel('authors'),
-    ] + Page.content_panels
+        ImageChooserPanel('header_image'),
+        FieldPanel('excerpt'),
+        FieldPanel('body'),
+    ]
+
+    class Meta:
+        verbose_name = 'Post'
