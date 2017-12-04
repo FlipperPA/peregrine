ace21940c4af640018df13fed9661401528a4a7d
Allen, Timothy <tallen@wharton.upenn.edu>
2017-12-04 15:34:31 +0000

    Proper ordering.

--- peregrine/models.py
+++ peregrine/models.py
@@ -18,11 +18,11 @@ class Category(models.Model):
 
     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
         verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
 
 
 class SitePage(ContentStreamPage):
