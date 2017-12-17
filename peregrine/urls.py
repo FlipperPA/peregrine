854c07fcb0e42d53a75cf8d5a4f721f5679915bd
Allen, Timothy <tallen@wharton.upenn.edu>
2017-12-17 03:00:53 +0000

    Add an RSS feed.

--- peregrine/urls.py
+++ peregrine/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url, include
 
 from wagtail.core import urls as wagtail_urls
 
-from .views import PostListView
+from .views import PostsListView, PostsFeed
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view()),
+    url(r'rss/$', PostsFeed()),
+    url(r'^$', PostsListView.as_view()),
     url(r'', include(wagtail_urls)),
 ]
--- peregrine/views.py
+++ peregrine/views.py
@@ -1,10 +1,35 @@
+from django.contrib.syndication.views import Feed
 from django.views.generic import ListView
 
 from .models import SitePost
 
 
-class PostListView(ListView):
+class PostsListView(ListView):
+    """
+    Paginated view of blog posts.
+    """
     model = SitePost
     template_name = 'peregrine/site_post_list.html'
     context_object_name = 'posts'
     paginate_by = 10
+
+
+class PostsFeed(Feed):
+    """
+    RSS feed to blog posts.
+    """
+    title = "My blog articles"
+    link = "/blogs-feed/"
+    description = "All of my blogs as they are published"
+
+    def items(self):
+        return SitePost.objects.live().order_by('-post_date')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.excerpt
+
+    def item_link(self, item):
+        return item.full_url
