a3401d73e0fddcbe359dd0f7115aa3850d12003e
Allen, Timothy <tallen@wharton.upenn.edu>
2018-05-27 15:37:55 +0000

    Start code for deletion of old revisions.

--- peregrine/wagtail_hooks.py
+++ peregrine/wagtail_hooks.py
@@ -15,3 +15,17 @@ class CategoryAdmin(ModelAdmin):
 
 
 modeladmin_register(CategoryAdmin)
+
+
+@hooks.register('after_edit_page')
+def delete_old_revisions(request, page):
+    """
+    This will clear Django's entire cache after a page edit. It is ugly,
+    but Django's cache mechanism doesn't currently support a way to easily
+    depending on the value of is_staff() and (if present) is_faculty.
+    """
+
+    revisions_to_keep = revisions_to_keep()
+    if revisions_to_keep is not None:
+        pks_to_delete = page.revisions.order_by('-created_at')[revisions_to_keep:].values_list('id', flat=True)
+        page.revisions.filter(pk__in=pks_to_delete).delete()
