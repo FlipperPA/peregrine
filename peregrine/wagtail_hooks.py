ef0d00cea419304ed78cec7b00b9e18fdbe06bfb
Allen, Timothy <tallen@wharton.upenn.edu>
2018-05-30 00:06:34 +0000

    Test a vacuum after a page save.

--- peregrine/wagtail_hooks.py
+++ peregrine/wagtail_hooks.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.db import connection
+
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.core import hooks
 
@@ -29,3 +32,9 @@ def delete_old_revisions(request, page):
     if peregrine_settings.revisions_to_keep is not None:
         pks_to_delete = page.revisions.order_by('-created_at')[peregrine_settings.revisions_to_keep:].values_list('id', flat=True)
         page.revisions.filter(pk__in=pks_to_delete).delete()
+
+        # If running Zappa with S3 SQLite, perform a vacuum to keep the database size small.
+        if settings.DATABASES['default']['ENGINE'] == 'zappa_django_utils.db.backends.s3sqlite':
+            cursor = connection.cursor()
+            cursor.execute('VACUUM;')
+            cursor.close()
